26847ffc519b790be35f763bb2f397cc803e9f42
Mohd Aman Patel <ap5277478@gmail.com>
2024-08-01 22:49:18 +0530

    Update UserDetails.jsx

--- client/src/components/UserDetails.jsx
+++ client/src/components/UserDetails.jsx
@@ -18,11 +18,10 @@ const UserDetails = () => {
   const [filePreview, setFilePreview] = useState(null);
   const [messages, setMessages] = useState([]);
   const [roomId, setRoomId] = useState("");
-  const socket = io("https://my-social-media-v6xp.onrender.com/", {
+  const socket = io("https://my-social-media-v6xp.onrender.com", {
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
   timeout: 5000,
-  transports: ["websocket"], 
 });
 
 
