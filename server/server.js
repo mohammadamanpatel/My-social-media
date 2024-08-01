fd70c88a6eb8180fdd790facc0bd3c945e31b774
Mohd Aman Patel <ap5277478@gmail.com>
2024-08-01 22:47:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
  cors: {
-    origin: ["https://my-social-media-v6xp.onrender.com/"],
+    origin: ["https://my-social-media-v6xp.onrender.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
